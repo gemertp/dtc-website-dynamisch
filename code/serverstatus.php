4e7d28df456a55d627a81e7aa64a8b89ee88c6a2
Peter van Gemert <peter@petervg.nl>
2023-08-13 00:51:03 +0200

    Add files via upload

---
+++ code/serverstatus.php
@@ -0,0 +1,33 @@
+<?php
+    echo "<!DOCTYPE html>";
+
+    echo "<html>";
+    
+    echo "<head>";
+    echo "  <title>Serverstatus</title>";
+    echo "  <link type=\"stylesheet\" href=\"/serverstatus.css\">";
+    echo "</head>";
+
+    echo "<body>";
+
+    echo "<h1>Server status</h1>";
+    echo "<hr>";
+
+    echo "<h2>Servertijd</h2>";
+    echo "<p>";
+    $servertijd = shell_exec('date');
+    echo "$servertijd";
+    echo "</p>";
+    
+    echo "<h2>Uptime</h2>";
+    echo "<p>";
+    $uptime = shell_exec('/usr/bin/uptime');
+    echo "$uptime";
+    echo "</p>";
+    
+    echo "<h2>Running processes</h2>";
+    echo "<p>";
+    $runproc = shell_exec('/usr/bin/ps -ef | /usr/bin/wc -l');
+    echo "$runproc";
+    echo "</p>";
+?>
